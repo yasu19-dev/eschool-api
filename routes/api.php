2b789d464c01dbc93c246d53a00631c64bcf139c
ayabelghazi <belghaziaya92@gmail.com>
2026-04-04 16:06:45 +0100

    Architecture des Routes API (aya)

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,51 @@
-<?php
+    <?php
 
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AuthController;
+    use App\Http\Controllers\AbsenceController;
+    use App\Http\Controllers\SeanceController;
+    use App\Http\Controllers\UserController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 
 
-Route::get('/test-connexion', function () {
-    return response()->json([
-        'statut' => 'Succès',
-        'message' => 'L\'API de la plateforme E-School est bien connectée à React !'
-    ]);
-});
+    Route::get('/test-connexion', function () {
+        return response()->json([
+            'statut' => 'Succès',
+            'message' => 'L\'API de la plateforme E-School est bien connectée à React !'
+        ]);
+    });
 
-Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+
+     //  ESPACE STAGIAIRE
+    Route::prefix('stagiaire')->group(function () {
+        Route::get('/absences', [AbsenceController::class, 'myAbsences']);
+        Route::get('/schedule', [SeanceController::class, 'forStagiaire']);
+    });
+
+    //  ESPACE FORMATEUR
+    Route::prefix('formateur')->group(function () {
+        Route::get('/seances', [SeanceController::class, 'index']);
+        Route::post('/absences', [AbsenceController::class, 'store']); // Saisie
+    });
+
+    //  ESPACE DIRECTION
+    Route::prefix('director')->group(function () {
+        Route::get('/stats/global', [AbsenceController::class, 'globalStats']);
+        Route::apiResource('users', UserController::class); // CRUD complet
+    });
+
+    //  ESPACE RESPONSABLE STAGIAIRE
+    Route::prefix('responsable-stagiaire')->group(function () {
+        Route::get('/justifications', [AbsenceController::class, 'pendingJustifications']);
+        Route::patch('/absences/{id}', [AbsenceController::class, 'validateJustification']);
+    });
